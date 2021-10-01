c4dfc14a7cd13c40f65322f74016693f49ad4aab
asibs <andrew@organise.network>
2021-10-01 17:05:20 +0100

    Add support for the new recently_updated & recently_taken actions endpoints

--- lib/identity-api-client/actions.rb
+++ lib/identity-api-client/actions.rb
@@ -8,5 +8,29 @@ module IdentityApiClient
         false
       end
     end
+
+    def recently_updated(hours_ago = 4)
+      resp = client.get_request("/api/actions/recently_updated?api_token=#{api_token}&hours_ago=#{hours_ago}")
+      if resp.status == 200
+        return resp.body
+      else
+        false
+      end
+    end
+
+    def recently_taken(hours_ago = 4)
+      resp = client.get_request("/api/actions/recently_taken?api_token=#{api_token}&hours_ago=#{hours_ago}")
+      if resp.status == 200
+        return resp.body
+      else
+        false
+      end
+    end
+
+    private
+
+    def api_token
+      client.connection.configuration.options[:api_token]
+    end
   end
 end
