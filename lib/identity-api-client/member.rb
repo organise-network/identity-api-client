73a16f96930b2b33c269b8d4fb640d8a9ab526cd
asibs <andrew@organise.network>
2022-01-21 14:34:52 +0000

    As six/net 267/add path to validate identity login token for network (#8)
    * Add method to validate Identity login tokens
    
    * Add user_ip to verify login token API method
    
    * Add documentation comment
    
    Co-authored-by: orgsix <six@organise.org.uk>

--- lib/identity-api-client/member.rb
+++ lib/identity-api-client/member.rb
@@ -56,5 +56,24 @@ module IdentityApiClient
         return false
       end
     end
+
+    # Return Identity member_guid if the API call succeeded and the provided
+    # login token was valid; false otherwise
+    #
+    # Note that Identity returns the member_guid as a JSON object / hash, eg:
+    # { member_guid: '1234567890' }
+    def validate_login_token(login_token, user_ip = nil)
+      payload = {
+        login_token: login_token,
+        user_ip: user_ip,
+        api_token: client.connection.configuration.options[:api_token]
+      }
+      resp = client.post_request("/api/member_login/verify_login_token", payload)
+      if resp.status == 200
+        return resp.body
+      else
+        return false
+      end
+    end
   end
 end
