5deae4965b702f457d11dc222eb473abe3248d72
asibs <sibsvandal@hotmail.com>
2022-02-08 17:04:01 +0000

    Add workplace#update_single_member API method (#9)
    * Add workplace#update_single_member API method
    
    * Bugfix

--- lib/identity-api-client/workplaces.rb
+++ lib/identity-api-client/workplaces.rb
@@ -30,5 +30,32 @@ module IdentityApiClient
         return resp.body['errors']
       end
     end
+
+    def update_single_member(email:, created_at:, originator:, **employment_data)
+      raise(ArgumentError, 'email must be present & non-nil') unless email
+
+      required_employment_data = %i{employment_status employer industry profession} # Must have at least one of these
+      permitted_employment_data = required_employment_data.concat(%i{other_emp_status})
+
+      if required_employment_data.none? { |key| employment_data.key?(key) }
+        raise(ArgumentError, 'at least one of employment_status, employer, industry or profession must be passed')
+      end
+
+      payload = employment_data.slice(*permitted_employment_data).transform_keys(&:to_s)
+      payload.merge!({
+        'email' => email,
+        'created_at' => created_at,
+        'originator' => originator,
+        'api_token' => client.connection.configuration.options[:api_token]
+      })
+
+      resp = client.post_request('/api/workplaces/update_single_member', payload)
+
+      if resp.status == 200
+        return resp.body
+      else
+        return false
+      end
+    end
   end
 end
