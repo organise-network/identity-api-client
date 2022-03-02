e020ae765a682e68f3097f88bfc55816ed73419d
orgsix <six@organise.org.uk>
2022-03-02 14:21:00 +0000

    Make default mailing API base path '/mailings'

--- lib/identity-api-client/mailing_api_base.rb
+++ lib/identity-api-client/mailing_api_base.rb
@@ -1,7 +1,7 @@
 module IdentityApiClient
 	class MailingApiBase < Base
 		def route_url(url)
-			path = client.connection.configuration.options[:mailing_api_path]
+			path = client.connection.configuration.options[:mailing_api_path] || '/mailings'
 			"#{path}#{url}"
 		end
 	end
