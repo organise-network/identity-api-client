06ec35eff120e010b83c4b720aa00d6ef5a56510
Dan Garland <dan@organise.network>
2023-08-10 10:04:15 +0100

    remove hack to allow current vertebrae version

--- lib/identity-api-client.rb
+++ lib/identity-api-client.rb
@@ -1,7 +1,4 @@
 require 'vertebrae'
-# dirty patch
-spec = Gem::Specification.find_by_name("vertebrae")
-require spec.gem_dir + '/lib/api.rb'
 require 'identity-api-client/base'
 require 'identity-api-client/mailing_api_base'
 
